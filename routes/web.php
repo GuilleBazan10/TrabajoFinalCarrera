edada33a82aa3dab34f1d34e9b47e5761f75d02a
cheuco1904 <maxi.vergara5@gmail.com>
2022-07-07 19:42:59 -0300

    Negro 2

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 
-Route::get('/bebidas' function() {
-    return view('bebidas')}
-
-);
 });
 
 Route::get('/bebidas', function () {
