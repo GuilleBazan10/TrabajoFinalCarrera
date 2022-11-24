106650baaf5395efc68b6e6bfd399ee1e054580e
cheuco1904 <maxi.vergara5@gmail.com>
2022-11-24 10:11:40 -0300

    'Creacion de Login'

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,15 @@ Route::get('/bebidas', function () {
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
