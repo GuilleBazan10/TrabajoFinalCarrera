909b68dcdf98342a635458143a681d565ba9beae
cheuco1904 <maxi.vergara5@gmail.com>
2023-08-29 23:16:34 -0300

    Error Front

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+            <div class="card border-success">
+                <div class="card-header bg-success text-white">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -77,7 +77,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-success">
                                     {{ __('Register') }}
                                 </button>
                             </div>
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,12 @@ use App\Http\Controllers\AuthController;
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 
-});
+}); 
 
 Route::get('/bebidas', function () {
         $titulo = "Listado de Bebidas";
@@ -27,22 +29,25 @@ Route::get('/bebidas', function () {
 
 });
 
+// Route::get('/login', function () {
+//     return view('auth.login');
+// })->name('login');
+
+// Route::get('/register', function () {x  z       z   
+//     return view('auth.register');
+// })->name('register');
 
 
 
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+//Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+
+
+
